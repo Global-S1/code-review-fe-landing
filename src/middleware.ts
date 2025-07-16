0461a98b3ee36b55ea97f8233629117e070e575f
JulianGS1 <julian.agama@globals.one>
2025-07-16 17:28:36 -0500

    hot-fix (home): add middleware (#15)
    * feat: 🎸 add middleware to redirect
    
    * fix: 🐛 dele comment

---
+++ src/middleware.ts
@@ -0,0 +1,8 @@
+import { defaultLang } from "@i18n/config/languaje";
+import { defineMiddleware } from "astro/middleware";
+
+export const onRequest = defineMiddleware(({ request, redirect, url }) => {
+  if (url.pathname === "/") {
+    return redirect(`/${defaultLang}`);
+  }
+});
